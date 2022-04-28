0fe1c99bdbc628d3ce3a39f2aaaf24a47266dc6b
JC <ezjcd101@gmail.com>
2022-04-28 19:42:00 +0800

    modified models

--- FoodWarehouseAPI/models.py
+++ FoodWarehouseAPI/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class fstock(models.Model):
+    Mfd_id = models.IntegerField(primary_key=True)
+    Name = models.CharField(max_length=50)
+    UnitPrice = models.DecimalField(max_digits=5, decimal_places=2)
+    UnitsInStock = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.Name
+
